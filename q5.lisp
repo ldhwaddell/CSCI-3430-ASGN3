2daecbe8c6f53a8f0c61b7e392bad83f90f0a005
ldhwaddell <ldhwaddell@gmail.com>
2022-10-09 14:05:20 -0300

    add validation for second/third letters

--- q5.lisp
+++ q5.lisp
@@ -11,7 +11,7 @@
     (nil)))
 
 (defun validateList (l)
-  ; return true if list i exactly 6 elements long and each one is not a list
+  ; return true if list is 6 elements long and each one is not a list
   (if (and (checkLength l) (every #'eq (mapcar #'notlist l) '(T T T T T T)))
   t
   nil))
@@ -22,17 +22,24 @@
   t 
   nil))
 
-(defun containsLetter (element)
+(defun validateFirstLetter (element)
 ; check that element is in the set of valid letters) 
 (if (member element `(A B C D E F G H I J)) 
+  t 
+  nil))
+
+(defun validateLetters (element)
+; check that second and third letters are single letters in alphabet
+(if (member element `(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)) 
   t 
   nil))
 
 (defun NSLicense (l)
   ; check that list is of valid length and contains no sublists
   (if (validateList l)
-  ; if the first 3 items are valid letters and second 3 are valid numbers, return true
+  ; if first 3 items are valid letters and second 3 are valid numbers, return true
   (cond ((and 
-    (every #'eq (mapcar #'containsLetter (subseq l 0 3)) '(T T T)) 
+    (every #'eq (mapcar #'validateFirstLetter (subseq l 0 1)) '(T))
+    (every #'eq (mapcar #'validateLetters (subseq l 1 3)) '(T T)) 
     (every #'eq (mapcar #'containsNum (subseq l 3 6)) '(T T T))) t))
   nil))
